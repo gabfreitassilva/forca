d65e731f52bc0c02cffadfd93f3d5b27131ecf33
Gabriel Freitas <65826668+gabfreitassilva@users.noreply.github.com>
2022-10-20 07:51:57 -0300

    mudanças de nomes e aglumas melhorias

--- main.cpp
+++ main.cpp
@@ -1,82 +1,85 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <random>
 #include <string>
 #include <vector>
 
 class Game {
 	private:
-		std::vector<std::string> dictionary;
-		std::vector<std::string> wrong_words;
-		std::string word, secret_word, user_word;
-		int random_line, chance = 6, word_size;
-		bool win = false, letter_exist = false;
+		std::vector<std::string> dicionario;
+		std::vector<std::string> palavras_erradas;
+		std::string palavra, palavra_secreta, palavra_usuario;
+		int linha_aleatoria, chance = 6, tamanho_palavra;
+		bool vitoria = false, letra_existe = false;
 
 	public:
-		std::string random_word (void);
-		void display_game(void);
-		void end_game(void);
-		void wrong_answers(void);
+		std::string random_word ();
+		void display_game();
+		void end_game();
+		void wrong_answers();
 };
 
-std::string Game::random_word(void) {
+std::string Game::random_word() {
 	std::ifstream read;
 	read.open("palavras.txt");
 	if (read.is_open()) {
-		while (std::getline(read, word))
-			dictionary.push_back(word); // armazenar cada linha do arquivo "palavras.txt" no vector dictionary
-		srand(time(0));
-		random_line = rand()%dictionary.size();
-		word = dictionary.at(random_line); // escolhe uma palavra aleatória dentro do vector dicitonary
+		while (std::getline(read, palavra))
+			dicionario.push_back(palavra); // armazenar cada linha do arquivo "palavras.txt" no vector dictionary
+		std::random_device rd;
+    	std::mt19937 mt(rd());
+    	std::uniform_int_distribution<int> dist(0,79);
+    	linha_aleatoria = dist(mt);
+		palavra = dicionario[linha_aleatoria];
+		palavra.resize(5);
 		read.close();
 	}
-	return word;
+	return palavra;
 }
 
-void Game::wrong_answers(void) {
+void Game::wrong_answers() {
 	std::cout << "Palavras erradas:\n";
-	if (user_word != word)
-		wrong_words.push_back(user_word);
-	for (int i = 0; i < wrong_words.size(); i++)
-		std::cout << wrong_words[i] << '\n';
+	if (palavra_usuario != palavra)
+		palavras_erradas.push_back(palavra_usuario);
+	for (int i = 0; i < palavras_erradas.size(); i++)
+		std::cout << palavras_erradas[i] << '\n';
 }
 
-void Game::display_game(void) {
-	secret_word = random_word();
-	word_size = secret_word.size();
-	for (int i = 0; i < word_size; i++)
-		secret_word[i] = '-'; // escondendo a palavra da vez com caractere especial '-'
+void Game::display_game() {
+	palavra_secreta = random_word();
+	tamanho_palavra = palavra_secreta.length();
+	for (int i = 0; i < tamanho_palavra; i++)
+		palavra_secreta[i] = '-'; // escondendo a palavra da vez com caractere especial '-'
 	do {
 		system("clear"); // limpar terminal (mudar de acordo com o sistem operacional)
 		std::cout << "Voce tem " << chance << " chances\n";
-		std::cout << "Acerte a palavra: " << secret_word << '\n';
+		std::cout << "Acerte a palavra: " << palavra_secreta << '\n';
 		wrong_answers();
 		std::cout << "\nSeu palpite: ";
-		std::cin >> user_word;
-		for (int j = 0; j < word_size; j++) {
-			for (int k = 0; k < word_size; k++) {
-				if (user_word[j] == word[k])
-					secret_word[k] = user_word[j]; // testar letra por letra da palavra digtada entre a palavra do jogo
+		std::cin >> palavra_usuario;
+		for (int j = 0; j < tamanho_palavra; j++) {
+			for (int k = 0; k < tamanho_palavra; k++) {
+				if (palavra_usuario[j] == palavra[k])
+					palavra_secreta[k] = palavra_usuario[j]; // testar letra por letra da palavra digtada entre a palavra do jogo
 			}
 		}
-		if (secret_word != word)
+		if (palavra_secreta != palavra)
 			chance--;
 		else
-			win = true;
-	} while (chance > 0 && win != true);
+			vitoria = true;
+	} while (chance > 0 && vitoria != true);
 }
 
-void Game::end_game(void) {
+void Game::end_game() {
 	system("clear"); // limpar terminal (mudar de acordo com o sistem operacional)
 	if (chance == 0) {
 		std::cout << "Voce tem " << chance << " chances\n";
-		std::cout << "A palavra era: " << word << '\n';
+		std::cout << "A palavra era: " << palavra << '\n';
 		wrong_answers();
 		std::cout << "\nMais sorte na proxima vez...\n";
 	}
-	if (win == true) {
+	if (vitoria == true) {
 		std::cout << "Voce tem " << chance << " chances\n";
-		std::cout << "Acerte a palavra: " << secret_word << '\n';
+		std::cout << "Acerte a palavra: " << palavra_secreta << '\n';
 		wrong_answers();
 		std::cout << "\nParabens, voce venceu o jogo!!!\n";
 	}
